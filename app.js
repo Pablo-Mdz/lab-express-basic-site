52e21ce0947e9e9d2ee902ffa64bacb350149f0e
Pablo Cigoy <pablocigoy@gmail.com>
2022-11-08 23:12:36 +0100

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+app.get('/photo-galery', (request, response, next) => response.sendFile(__dirname + '/views/photo-galery.html'));
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+
+// Server Started
+app.listen(3000, () => console.log('My first app listening on port 3000!'));
